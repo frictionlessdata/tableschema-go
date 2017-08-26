7bed311724d602d033d8a54f14fa40d3f79b7d3b
Daniel Fireman <danielfireman@gmail.com>
2017-08-26 10:20:43 -0300

    Adding infer example with internationalization

---
+++ examples/infer/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/frictionlessdata/tableschema-go/csv"
+)
+
+type user struct {
+	ID   int
+	Age  int
+	Name string
+}
+
+func main() {
+	table, err := csv.New(csv.FromFile("data_infer_utf8.csv"), csv.SetHeaders("id", "age", "name"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := table.Infer(); err != nil {
+		log.Fatal(err)
+	}
+	// Writing schema to stdout.
+	table.Schema.Save(os.Stdout)
+
+	var data []user
+	if err := table.CastAll(&data); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("\n\nData:%+v\n", data)
+}
