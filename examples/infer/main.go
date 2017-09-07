5c2a39eba0f1ceb66bfffd3145982c71b1e43648
Daniel Fireman <danielfireman@gmail.com>
2017-09-07 19:05:21 -0300

    Making DecodeTable a method of Schema, code reads better

--- examples/infer/main.go
+++ examples/infer/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/frictionlessdata/tableschema-go/csv"
 	"github.com/frictionlessdata/tableschema-go/schema"
 )
@@ -14,33 +12,14 @@ type user struct {
 }
 
 func main() {
-	table, err := csv.NewTable(csv.FromFile("data_infer_utf8.csv"), csv.SetHeaders("id", "age", "name"))
+	tab, err := csv.NewTable(csv.FromFile("data_infer_utf8.csv"), csv.SetHeaders("id", "age", "name"))
 	if err != nil {
 		panic(err)
 	}
-
-	userSchema, err := schema.Infer(table)
+	sch, err := schema.Infer(tab)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(userSchema)
-
-	iter, _ := table.Iter()
-	defer iter.Close()
-	for iter.Next() {
-		var data user
-		if err := userSchema.Decode(iter.Row(), &data); err != nil {
-			panic(err)
-		}
-		fmt.Println(data)
-	}
-
-	// decoder, err := schema.NewDecoder(table, userSchema)
-	// for decoder.More() {
-	// 	var data user
-	// 	if err := decoder.Decode(&data); err != nil {
-	// 		panic(err)
-	// 	}
-	// 	fmt.Println(data)
-	// }
+	var users []user
+	sch.DecodeTable(tab, &users)
 }
