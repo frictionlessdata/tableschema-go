d910107cdd0dc5be16792c79f849ff42ee5998cf
Daniel Fireman <danielfireman@gmail.com>
2017-09-08 15:20:13 -0300

    Adding encodeBoolean

--- schema/boolean.go
+++ schema/boolean.go
@@ -1,6 +1,9 @@
 package schema
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func castBoolean(value string, trueValues, falseValues []string) (bool, error) {
 	for _, v := range trueValues {
@@ -14,4 +17,23 @@ func castBoolean(value string, trueValues, falseValues []string) (bool, error) {
 		}
 	}
 	return false, fmt.Errorf("invalid boolean value:%s", value)
+}
+
+func encodeBoolean(value interface{}, trueValues, falseValues []string) (string, error) {
+	switch value.(type) {
+	case bool:
+		return fmt.Sprintf("%v", value), nil
+	case string:
+		for _, v := range trueValues {
+			if value == v {
+				return value.(string), nil
+			}
+		}
+		for _, v := range falseValues {
+			if value == v {
+				return value.(string), nil
+			}
+		}
+	}
+	return "", fmt.Errorf("invalid boolean - value:\"%v\" type:%v", value, reflect.ValueOf(value).Type())
 }
--- schema/boolean_test.go
+++ schema/boolean_test.go
@@ -31,3 +31,50 @@ func TestCastBoolean_Error(t *testing.T) {
 		t.Errorf("want:err got:nil")
 	}
 }
+
+func TestEncodeBoolean(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		data := []struct {
+			desc        string
+			value       interface{}
+			want        string
+			trueValues  []string
+			falseValues []string
+		}{
+			{"True", true, "true", []string{}, []string{}},
+			{"False", false, "false", []string{}, []string{}},
+			{"TrueFromTrueValues", "0", "0", []string{"0"}, []string{}},
+			{"FalseFromFalseValues", "1", "1", []string{}, []string{"1"}},
+		}
+		for _, d := range data {
+			t.Run(d.desc, func(t *testing.T) {
+				got, err := encodeBoolean(d.value, d.trueValues, d.falseValues)
+				if err != nil {
+					t.Fatalf("err want:nil got:%q", err)
+				}
+				if d.want != got {
+					t.Errorf("val want:%s got:%s", d.want, got)
+				}
+			})
+		}
+	})
+	t.Run("Error", func(t *testing.T) {
+		data := []struct {
+			desc        string
+			value       interface{}
+			trueValues  []string
+			falseValues []string
+		}{
+			{"InvalidType", 10, []string{}, []string{}},
+			{"NotInTrueOrFalseValues", "1", []string{}, []string{}},
+		}
+		for _, d := range data {
+			t.Run(d.desc, func(t *testing.T) {
+				_, err := encodeBoolean(d.value, d.trueValues, d.falseValues)
+				if err == nil {
+					t.Fatalf("err want:err got:nil")
+				}
+			})
+		}
+	})
+}
