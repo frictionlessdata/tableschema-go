1ae0f7f5c1b8e7ce34be8f8323e35c9caef26baa
hwchiu <hwchiu@linkernetworks.com>
2017-10-02 23:59:33 +0800

    add constraints to testing, i.e, UUID should longer than 36 byte

--- schema/string_test.go
+++ schema/string_test.go
@@ -4,30 +4,31 @@ import "testing"
 
 // To be in par with the python library.
 func TestDecodeString_URIMustRequireScheme(t *testing.T) {
-	if _, err := decodeString(stringURI, "google.com"); err == nil {
+	if _, err := decodeString(stringURI, "google.com", Constraints{}); err == nil {
 		t.Errorf("want:err got:nil")
 	}
 }
 
 func TestDecodeString_InvalidUUIDVersion(t *testing.T) {
 	// This is a uuid3: namespace DNS and python.org.
-	if _, err := decodeString(stringUUID, "6fa459ea-ee8a-3ca4-894e-db77e160355e"); err == nil {
+	if _, err := decodeString(stringUUID, "6fa459ea-ee8a-3ca4-894e-db77e160355e", Constraints{}); err == nil {
 		t.Errorf("want:err got:nil")
 	}
 }
 
 func TestDecodeString_Success(t *testing.T) {
 	var data = []struct {
-		Desc   string
-		Value  string
-		Format string
+		Desc        string
+		Value       string
+		Format      string
+		constraints Constraints
 	}{
-		{"URI", "http://google.com", stringURI},
-		{"Email", "foo@bar.com", stringEmail},
-		{"UUID", "C56A4180-65AA-42EC-A945-5FD21DEC0538", stringUUID},
+		{"URI", "http://google.com", stringURI, Constraints{}},
+		{"Email", "foo@bar.com", stringEmail, Constraints{}},
+		{"UUID", "C56A4180-65AA-42EC-A945-5FD21DEC0538", stringUUID, Constraints{MinLength: 36}},
 	}
 	for _, d := range data {
-		v, err := decodeString(d.Format, d.Value)
+		v, err := decodeString(d.Format, d.Value, d.constraints)
 		if err != nil {
 			t.Errorf("want:nil got:%q", err)
 		}
