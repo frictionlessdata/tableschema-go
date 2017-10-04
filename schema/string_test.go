ea0f0f63f3fe968cc954238d7d0b25e30b05db38
hwchiu <hwchiu@linkernetworks.com>
2017-10-04 21:25:35 +0800

    passing complied pattern to constraints rather than original pattern string in testing function

--- schema/string_test.go
+++ schema/string_test.go
@@ -32,20 +32,12 @@ func TestDecodeString_ErrorCheckingConstraints(t *testing.T) {
 		{"InvalidMaxLength_UUID", "6fa459ea-ee8a-3ca4-894e-db77e160355e", stringUUID, Constraints{MaxLength: 1}},
 		{"InvalidMaxLength_Email", "foo@bar.com", stringEmail, Constraints{MaxLength: 1}},
 		{"InvalidMaxLength_URI", "http://google.com", stringURI, Constraints{MaxLength: 1}},
-		{"InvalidPattern_UUID", "6fa459ea-ee8a-3ca4-894e-db77e160355e", stringUUID, Constraints{Pattern: "^[0-9a-f]{1}-.*"}},
-		{"InvalidPattern_Email", "foo@bar.com", stringEmail, Constraints{Pattern: "[0-9].*"}},
-		{"InvalidPattern_URI", "http://google.com", stringURI, Constraints{Pattern: "^//.*"}},
+		{"InvalidPattern_UUID", "6fa459ea-ee8a-3ca4-894e-db77e160355e", stringUUID, Constraints{compiledPattern: regexp.MustCompile("^[0-9a-f]{1}-.*")}},
+		{"InvalidPattern_Email", "foo@bar.com", stringEmail, Constraints{compiledPattern: regexp.MustCompile("[0-9].*")}},
+		{"InvalidPattern_URI", "http://google.com", stringURI, Constraints{compiledPattern: regexp.MustCompile("^//.*")}},
 	}
 	for _, d := range data {
 		t.Run(d.desc, func(t *testing.T) {
-			if d.constraints.Pattern != "" {
-				var err error
-				d.constraints.compiledRegexp, err = regexp.Compile(d.constraints.Pattern)
-				if err != nil {
-					t.Fatalf("compile Pattern to Regexp fail")
-				}
-			}
-
 			if _, err := decodeString(d.format, d.value, d.constraints); err == nil {
 				t.Fatalf("err want:err got:nil")
 			}
@@ -60,9 +52,9 @@ func TestDecodeString_Success(t *testing.T) {
 		format      string
 		constraints Constraints
 	}{
-		{"URI", "http://google.com", stringURI, Constraints{MinLength: 1, Pattern: "^http://.*"}},
-		{"Email", "foo@bar.com", stringEmail, Constraints{MinLength: 1, Pattern: ".*@.*"}},
-		{"UUID", "C56A4180-65AA-42EC-A945-5FD21DEC0538", stringUUID, Constraints{MinLength: 36, MaxLength: 36, Pattern: "[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{8}"}},
+		{"URI", "http://google.com", stringURI, Constraints{MinLength: 1, compiledPattern: regexp.MustCompile("^http://.*")}},
+		{"Email", "foo@bar.com", stringEmail, Constraints{MinLength: 1, compiledPattern: regexp.MustCompile(".*@.*")}},
+		{"UUID", "C56A4180-65AA-42EC-A945-5FD21DEC0538", stringUUID, Constraints{MinLength: 36, MaxLength: 36, compiledPattern: regexp.MustCompile("[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}")}},
 	}
 	for _, d := range data {
 		t.Run(d.desc, func(t *testing.T) {
