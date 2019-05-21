0a60b46c01bc0980dc9f32c442ef33f1c3633c1f
Daniel Fireman <danielfireman@gmail.com>
2019-05-20 22:44:55 -0300

    Fix and transform tests in table-driven tests

--- schema/schema_test.go
+++ schema/schema_test.go
@@ -665,24 +665,24 @@ func TestSchema_CastRow(t *testing.T) {
 
 func TestSchema_CastColumn(t *testing.T) {
 	sch := Schema{Fields: []Field{{Name: "Age", Type: IntegerType}}}
-	t.Run("ErrorArgMustBeSlicePointer", func(t *testing.T) {
-		is := is.New(t)
-		var f []string
-		is.True(sch.CastColumn([]string{"42"}, "Age", f) != nil) // Must err because f is not a pointer.
-	})
-	t.Run("ErrorInvalidFieldName", func(t *testing.T) {
-		is := is.New(t)
-		var f []string
-		is.True(sch.CastColumn([]string{"42"}, "Name", f) != nil) // Must err because Name is not a valid field.
-	})
-	t.Run("ErrorCanNotCast", func(t *testing.T) {
-		is := is.New(t)
-		var f []int
-		is.True(sch.CastColumn([]string{"Foo"}, "Age", f) != nil) // Must err because Foo can not be cast to integer.
-	})
-	t.Run("ErrorCanNotConvert", func(t *testing.T) {
-		is := is.New(t)
-		var f []string
-		is.True(sch.CastColumn([]string{"42"}, "Age", f) != nil) // Must err because 42 (integer) can not be converted to string.
+	t.Run("Errors", func(t *testing.T) {
+		data := []struct {
+			desc    string
+			col     []string
+			colName string
+			out     interface{}
+		}{
+			{"OutNotASlicePointer", []string{"42"}, "Age", []int{}},
+			{"InvalidFieldName", []string{"42"}, "Name", &([]string{})},
+			{"CanNotCast", []string{"Foo"}, "Age", &([]int{})},
+			{"CanNotConvert", []string{"42"}, "Age", &([]*int{})},
+		}
+		for _, d := range data {
+			t.Run(d.desc, func(t *testing.T) {
+				if sch.CastColumn(d.col, d.colName, d.out) == nil {
+					t.Errorf("want:err got:nil col:%v colName:%v out:%v", d.col, d.colName, d.out)
+				}
+			})
+		}
 	})
 }
