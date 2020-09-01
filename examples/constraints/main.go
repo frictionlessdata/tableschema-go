3cc64ed28c51c890b722e708d1c1874e16c2ae5f
danielfireman <danielfireman@gmail.com>
2020-09-01 20:16:34 -0300

    Constraints example.

---
+++ examples/constraints/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+
+	"github.com/frictionlessdata/tableschema-go/csv"
+	"github.com/frictionlessdata/tableschema-go/schema"
+)
+
+// Example of how to read, validate and change a schema.
+func main() {
+	// Reading schem.
+	capitalSchema, err := schema.LoadFromFile("schema.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Validate schema.
+	if err := capitalSchema.Validate(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Printing schema fields names.
+	log.Println("Fields:")
+	for i, f := range capitalSchema.Fields {
+		log.Printf("%d - %s\n", i, f.Name)
+	}
+
+	// Working with schema fields.
+	if capitalSchema.HasField("Capital") {
+		log.Println("Field capital exists in schema")
+	} else {
+		log.Fatalf("Schema must have the field capital")
+	}
+	field, _ := capitalSchema.GetField("URL")
+	if field.TestString("http://new.url.com") {
+		value, err := field.Cast("http://new.url.com")
+		log.Printf("URL unmarshal to value: %v\n", value)
+		if err != nil {
+			log.Fatalf("Error casting value: %q", err)
+		}
+	} else {
+		log.Fatalf("Value http://new.url.com must fit in field capital.")
+	}
+
+	// Dealing with tabular data associated with the schema.
+	table, err := csv.NewTable(csv.FromFile("capital.csv"), csv.LoadHeaders())
+	var capitals = []struct {
+		ID      int
+		Capital float64
+		URL     string
+	}{}
+
+	if err := capitalSchema.CastTable(table, &capitals); err != nil {
+		log.Fatalf("Error casting table: %q", err)
+	}
+	log.Printf("Cast Table: %+v\n", capitals)
+}
